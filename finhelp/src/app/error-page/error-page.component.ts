9454e27e56554934bf9a3a96721e1cc6fa6b662e
Artem <artemvandalovski@gmail.com>
2023-01-21 16:06:00 -0500

    home

--- finhelp/src/app/error-page/error-page.component.ts
+++ finhelp/src/app/error-page/error-page.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error-page',
@@ -7,9 +6,5 @@ import { Router } from '@angular/router';
   styleUrls: ['./error-page.component.css']
 })
 export class ErrorPageComponent {
-	constructor(private router: Router) {}
-	  
-	goHome() {
-		this.router.navigate(['./']);
-	}
+
 }
