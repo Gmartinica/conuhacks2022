e6847ee3a08a69e864759d8a1e1c0853ec92f909
shouvik <shouvikdeebee@gmail.com>
2023-01-21 14:55:27 -0500

    added the form with a bunch of temporary validators, will make it look nicer now and will add the post request

--- finhelp/src/app/app.module.ts
+++ finhelp/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InfoFormComponent } from './info-form/info-form.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { InfoFormComponent } from './info-form/info-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
