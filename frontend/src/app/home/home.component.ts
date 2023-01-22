a889fc8ca2c791a6258927fa354cc0de5da6b7c0
Artem <artemvandalovski@gmail.com>
2023-01-22 06:12:03 -0500

    show user

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,3 +1,5 @@
+
+import { AuthService } from '@auth0/auth0-angular';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  constructor(public auth: AuthService) {}
 }
